0a381c5fedb738fe28f78f812af2edf927180767
Yigal Duppen <yigal@publysher.nl>
2012-10-20 11:16:19 +0200

    Integer IDs
    Namespaces and named graphs are not created a lot – using UUIDs would be overkill

--- src/rdflib_django/models.py
+++ src/rdflib_django/models.py
@@ -15,7 +15,6 @@ class NamedGraph(models.Model):
     Models a context which represents a named graph.
     """
 
-    id = UUIDField(verbose_name=_("ID"), primary_key=True)
     identifier = fields.URIField(verbose_name=_("Identifier"), unique=True)
 
     class Meta:
@@ -24,6 +23,27 @@ class NamedGraph(models.Model):
 
     def __unicode__(self):
         return u"{0}".format(self.identifier, "identifier")
+
+
+class NamespaceModel(models.Model):
+    """
+    A namespace definition.
+
+    In essence, a namespace consists of a prefix and a URI. However, the namespaces in rdflib_django
+    also have an extra field called '`fixed`' - this is used to mark namespaces that cannot be
+    remapped such as ``xml``, ``rdf`` and ``rdfs``.
+    """
+
+    prefix = models.CharField(max_length=50, verbose_name=_("Prefix"), unique=True)
+    uri = models.CharField(max_length=500, verbose_name=_("URI"), db_index=True, unique=True)
+    fixed = models.BooleanField(verbose_name=_("Fixed"), editable=False, default=False)
+
+    class Meta:
+        verbose_name = _("namespace")
+        verbose_name_plural = _("namespaces")
+
+    def __unicode__(self):
+        return "@prefix {0}: <{1}>".format(self.prefix, self.uri)
 
 
 class Resource(models.Model):
@@ -86,25 +106,3 @@ class LiteralPredicate(models.Model):
         Converts this predicate to a triple.
         """
         return self.subject.identifier, self.predicate, self.object
-
-
-class NamespaceModel(models.Model):
-    """
-    A namespace definition.
-
-    In essence, a namespace consists of a prefix and a URI. However, the namespaces in rdflib_django
-    also have an extra field called '`fixed`' - this is used to mark namespaces that cannot be
-    remapped such as ``xml``, ``rdf`` and ``rdfs``.
-    """
-
-    id = UUIDField(_("ID"), primary_key=True)
-    prefix = models.CharField(max_length=50, verbose_name=_("Prefix"), unique=True)
-    uri = models.CharField(max_length=500, verbose_name=_("URI"), db_index=True, unique=True)
-    fixed = models.BooleanField(verbose_name=_("Fixed"), editable=False, default=False)
-
-    class Meta:
-        verbose_name = _("namespace")
-        verbose_name_plural = _("namespaces")
-
-    def __unicode__(self):
-        return "@prefix {0}: <{1}>".format(self.prefix, self.uri)
